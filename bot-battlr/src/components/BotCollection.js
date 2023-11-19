98940c64caffb00c1e07801f6c56fa74134968cb
KennedyShiku <kenrichshiku@gmail.com>
2023-11-19 21:10:08 +0300

    Added styling for collected bots

--- bot-battlr/src/components/BotCollection.js
+++ bot-battlr/src/components/BotCollection.js
@@ -5,19 +5,19 @@ const BotCollection = ({selectedBots}) => {
   return ( 
     <div className="bot-collection">
       <h2>Bot Collection</h2>
-      <ul>
+      <div className="bot-cards">
         {selectedBots && selectedBots.map((bot, index) => (
-          <li key={bot.id}>
+          <div className="bot-card" key={bot.id}>
+            <h2>Name: {bot.name}</h2>
             <img src={bot.avatar_url} alt={bot.name} className="bot-image"/>
-            <h2>{bot.name}</h2>
             <p style={{fontSize:'12px', fontWeight: 'bold'}}>Catchphrase💬: {bot.catchphrase}</p>
             <p>Health❤️: {bot.health}</p>
             <p>Type🤖: {bot.bot_class}</p>
             <p>Damage💔: {bot.damage}</p>
             <p>Armor🛡️: {bot.armor}</p>
-          </li>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
    );
 }
